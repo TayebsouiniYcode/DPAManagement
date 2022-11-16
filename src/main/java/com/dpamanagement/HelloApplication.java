685e21c7cba24ca0d09512555052a5749b07f786
TayebsouiniYcode <TayebsouiniYcode@gmail.com>
2022-11-16 09:49:40 +0100

    initial commit

---
+++ src/main/java/com/dpamanagement/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.dpamanagement;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
